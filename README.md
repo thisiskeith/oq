8b31901cae2f653edd77929d232e162a79ac008a
Keith Hansen <designsbykeith@gmail.com>
2015-03-12 11:40:56 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # oXHR
+
+oXHR is a simple Oboe.js XHR wrapper with promises by Q optimized for use with your Node.js and React apps.
+
+## Examples
+
+React
+
+```js
+
+var xhrData = {
+        url: 'http://example.com/'
+    };
+
+return oXHR(xhrData);
+```
+
+Node.js
+
+```js
+exports.getExample = function (req, res) {
+
+    var xhrData = {
+        url: 'http://example.com/'
+    };
+
+    oXHR(xhrData)
+        .then(function (payload) {
+            res.send(payload);
+        })
+        .fail(function (error) {
+            // Handler error
+        })
+        .done();
+};
+```
+
+## Installation
+
+The easiest way to get started is to add the following to your `package.json` dependencies
+```js
+{
+    "dependencies": {
+        "oxhr": "git+ssh://git@github.com:thisiskeith/oXHR.git",
+    }
+}
+```
+Install the package
+```js
+npm install
+```
+Require in your application
+```
+var oXHR = require('oxhr');
+```
+
---
+++ oXHR.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var oboe = require('oboe');
+var Q = require('q');
+
+function oXHR(data) {
+
+    if (typeof data !== "object") {
+        throw new Error('data is undefined');
+    }
+
+    var defered = Q.defer();
+
+    var xhrObj = {
+            url: data.url,
+            method: data.method || 'GET',
+            cached: data.cached || false,
+            withCredentials: data.withCredentials || false
+        };
+
+    // Headers
+    if (data.headers) {
+        xhrObj.headers = data.headers;
+    }
+
+    // Body
+    if (data.body) {
+        xhrObj.body = data.body;
+    }
+
+    oboe(xhrObj)
+        .done(function (payload) {
+
+            if (data.callback) {
+                payload = data.callback(payload);
+            }
+
+            defered.resolve(payload);
+        })
+        .fail(function () {
+            defered.reject(new Error("Query failed"));
+        });
+
+    return defered.promise;
+}
+
+module.exports = oXHR;
